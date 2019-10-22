6fa38220777e7186ed3e74233ff992e792e99056
Jonny Lea <jonny@fractal-labs.com>
2019-10-22 14:56:38 +0100

    accounts post

--- functions/accounts/accounts.js
+++ functions/accounts/accounts.js
@@ -1,9 +1,12 @@
+var AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB();
+
 exports.get = async (event) => {}
 
 exports.connect = async (event) => {
     try {
         const accForm = event["body-json"];
-        return { 
+        const item = { 
             bankId: accForm.bankId,
             bankName: accForm.bankName,
             accountId: "fakeAcc113",
@@ -11,7 +14,13 @@ exports.connect = async (event) => {
             Identification: "GB83BANK43215378060931",
             Name: "Debit Account",
             dateAuthorised: "2018-09-06"
+        }
+        var toPut = {
+            Item: item, 
+            TableName: process.env.ACCOUNTS_TABLE
         };
+        await dynamo.putItem(toPut).promise();
+        return item;
     } catch (err) {
         throw new Error('[500] Internal Server Error');
     }
