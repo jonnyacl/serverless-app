3b10ba7b733a6b7be978fe9d03a4dba42a8f2327
Anjana S <anjana@fractal-labs.com>
2019-10-22 12:39:51 +0100

    lambdas

--- functions/payments/payments.js
+++ functions/payments/payments.js
@@ -1,1 +1,108 @@
-exports.pay = async (event) => {}
+var axios = require("axios");
+exports.pay = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 400,
+        body: JSON.stringify('Errors'),
+    };
+    const apiKey = 'xAezmUprBVaCPW9RmSq901Zhwo9ZF2Xg9PbZHXyE'
+    const contentType = 'application/json'
+    const partnerId = 'b90a254c452a4ad1b54921a27712765b'
+    const token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXJ0bmVyX25hbWUiOiJGcmFjdGFsIiwiaXNzIjoiQVBJLkZSQUNUQUwiLCJleHAiOjE1NjIxNzE0MTcsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJpYXQiOjE1NjIxNjk2MTcsImp0aSI6ImM5NzA5YzEwLWI2NDAtNCJ9.FyM7CEMeICSfPxPRk1-nPvwKLPBEMaD0r2xXC85E_rI'
+    const method = event.context['http-method']
+    const baseUrl = 'https://sandbox.fractal-dev.co.uk'
+    
+    if (method === 'GET') {
+      if(event.context['resource-path'] === '/transactions') {
+          return getTransactions()
+      }
+    }
+      
+    if(method === 'POST') {
+      if(event.context['resource-path'] === '/pay') {
+          return postDomesticPayments(event["body-json"])
+      }
+    }
+    
+  const getDomesticPayments = (bank, domesticPaymentId) => {
+      let url = baseUrl + '/' + bank + '/domestic-payments/' + domesticPaymentId
+      return getReq(url)
+  }
+  
+  const getFundsConfirmation = (bank, consentId) => {
+      let url = baseUrl + '/' + bank + '/domestic-payments-consents/' + consentId + '/funds-confirmation'
+      return getReq(url)
+  }
+  
+  const getConsentId = (bank, consentId) => {
+      let url = baseUrl + '/' + bank + '/domestic-payments-consents/' + consentId 
+      return getReq(url)
+  }
+  
+  const postDomesticPaymentsConsent = (bank, body) => {
+      let url = baseUrl + '/' + bank + '/domestic-payments-consents/'
+      return postReq(url, body)
+      
+  }
+  
+  const getTransactions = () => {
+    console.log('Attempting GET trx')
+      let url = baseUrl + '/' + 7 + '/fakeAccount' + '/transactions/'
+      console.log(url)
+      let trx =  getReq(url)
+      console.log(trx)
+      trx = trx.forEach(function (element) {
+        element.vat = "20%";
+      })
+      console.log(trx)
+      return trx
+  }
+  
+  const postDomesticPayments = (body) => {
+      const bank = body.bankId
+      const paymentBody = postDomesticPaymentsConsent(bank, body)
+      let url = baseUrl + '/' + bank + '/domestic-payments/'
+      let paymentpostresponse =  postReq(url, paymentBody);
+      return getDomesticPayments(bank, paymentpostresponse.Data.DomesticPaymentId)
+  }
+  
+  const getReq = (url) => {
+    const response =  axios.get(url, {
+        headers: {
+          Authorization: token,
+          'X-Api-Key': apiKey,
+          "X-Partner-Id": partnerId,
+          Accept: contentType,
+          "Content-Type": contentType,
+        }
+      }, function(res) {
+    console.log("Got response: " + res.statusCode);
+    context.succeed();
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+    context.done(null, 'FAILURE');
+  });
+  return response
+  }
+  
+  const postReq = (url, body) => {
+      const response = axios.post(url, body, {
+        headers: {
+          Authorization: token,
+          'X-Api-Key': apiKey,
+          "X-Partner-Id": partnerId,
+          Accept: contentType,
+          "Content-Type": contentType,
+        }
+      }, function(res) {
+    console.log("POST response: " + res.statusCode);
+    context.succeed();
+  }).on('error', function(e) {
+    console.log("POST error: " + e.message);
+    context.done(null, 'FAILURE');
+  });
+  return response
+  }
+
+    return response;
+}
--- functions/trx/trx.js
+++ functions/trx/trx.js
@@ -1,1 +1,50 @@
-exports.getTrx = async (event) => {}
+var axios = require("axios");
+exports.getTrx = async (event) => {
+    console.log('here')
+    // TODO implement
+    const response = ''
+    const apiKey = 'xAezmUprBVaCPW9RmSq901Zhwo9ZF2Xg9PbZHXyE'
+    const contentType = 'application/json'
+    const partnerId = 'b90a254c452a4ad1b54921a27712765b'
+    const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzYW5kYm94VXNlciIsIm5hbWUiOiJGcmFjQm94IiwiaWF0IjoxNTE2MjM5MDIyLCJleHBpcmVzIjoxODAwfQ.A-Xk_RwJu3BZQ7gsUgq7nK4UPJpqIKJtxbBxkz2eJU4'
+    const method = event.context['http-method']
+    const baseUrl = 'https://sandbox.fractal-dev.co.uk'
+    
+    if (method === 'GET') {
+      if(event.context['resource-path'] === '/transactions') {
+          return getTransactions()
+      }
+    }
+  
+  const getTransactions = () => {
+      let url = baseUrl + '/' + 7 + '/fakeAccount' + '/transactions/'
+      let trx =  getReq(url)
+      trx.forEach(function (element) {
+        element.vat = "20%";
+      });
+  }
+  
+  const getReq = (url) => {
+    const response = await axios.get(url, {
+        headers: {
+          Authorization: token,
+          'X-Api-Key': apiKey,
+          "X-Partner-Id": partnerId,
+          Accept: contentType,
+          "Content-Type": contentType,
+        }
+      })
+    .then(function (response) {
+      console.log(response);
+      if (response.items.length != 0) {
+      return response.items;
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });  
+  return response
+  }
+  
+    return response;
+}
