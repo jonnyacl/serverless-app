2ddd0f8d718a6a9dda35417268f59dbe5e9a68d6
Jonny Lea <jonny@fractal-labs.com>
2019-10-22 14:20:42 +0100

    tidy up async

--- functions/companies/companies.js
+++ functions/companies/companies.js
@@ -11,6 +11,7 @@ exports.connectCompany = async (event) => {
     const compForm = event["body-json"];
     const compName = compForm.companyName;
     const comp = await connectCompanyHouse(compName);
+    console.log(`COMP ${JSON.stringify(comp)}`);
     var params = {
       TableName: process.env.USER_TABLE,
       Key: {
@@ -33,49 +34,18 @@ exports.connectCompany = async (event) => {
 const connectCompanyHouse = async (companyName) => {
     const API_KEY = process.env.COMPANYHOUSE_KEY;
     const BASE_URI = "https://api.companieshouse.gov.uk/";
-    const companyNumber = await axios.get(BASE_URI+'search/companies', {
-    params: {
-      q: companyName
-    }
-  })
-  .then(function (response) {
-    console.log(response);
-    if (response.items.length != 0) {
-    return response.items[0].company_number;
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });  
-//   Probably buggy, still runs if previous call returned an error
-  if (companyNumber !== 'undefined') {
-    const companyData = await axios.get(BASE_URI+'search/company/'+companyNumber)
-    .then(function (response) {
-        console.log(response);
-        return response;
-    })
-    .catch(function (error) {
-      console.log(error);
+    let companyNumber;
+    const companyResp = await axios.get(`${BASE_URI}search/companies`, {
+      params: {
+        q: companyName
+      }
     });
-    return companyData;
-  }
-  else {
-    return {};
-  }
-}
-
-const deliverAddress = async (addressObject) => {
-  const addressList = Object.keys(addressObject).reduce(function(res, v) {
-    return res.concat(addressObject[v]);
-  }, []);
-  return addressList.join(" ");
-}
-
-const deliverYearEnd = async (yearEndObject) => {
-  // "day": "30",
-  // "month": "04"
-  const yearEndList = Object.keys(yearEndObject).reduce(function(res, v) {
-    return res.concat(yearEndObject[v]);
-  }, []);
-  return yearEndList.join("-");
+    console.log(companyResp.data);
+    if (companyResp.data.items.length != 0) {
+      companyNumber = companyResp.data.items[0].company_number;
+    } else {
+      throw new Error("[400] no company number");
+    } 
+    const companyData = await axios.get(`${BASE_URI}search/company/${companyNumber}`);
+    return companyData.data;
 }
